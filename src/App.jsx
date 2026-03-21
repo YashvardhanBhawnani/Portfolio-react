c19ec3516f649faf0fd6f2657ea2a975d2f14fa6
Yashvardhan Bhawnani <yashvardhanbhawnani@gmail.com>
2026-03-21 21:18:48 +0530

    feat:Add introAnimation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
 import "./App.css";
+import { useState } from "react";
+import IntroAnimation from "./IntroAnimation"
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Blurblob from "./Blurblob";
@@ -11,24 +12,30 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const [introDone, setIntroDone] = useState(false);
   return (
-    <div className="bg-[#050414]">
-      <Blurblob
-        position={{ top: "35%", left: "20%" }}
-        size={{ width: "30%", height: "40%" }}
-      />
-      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)" />
-      <div className="relative pt-10">
-        <Navbar />
-        <About />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Education />
-        <Contact />
-        <Footer />
-      </div>
-    </div>
+    <>
+      {!introDone && <IntroAnimation onFinish={() => setIntroDone(true)} />}
+      {introDone && (
+        <div className="bg-[#050414]">
+          <Blurblob
+            position={{ top: "35%", left: "20%" }}
+            size={{ width: "30%", height: "40%" }}
+          />
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)" />
+          <div className="relative pt-10">
+            <Navbar />
+            <About />
+            <Skills />
+            <Experience />
+            <Projects />
+            <Education />
+            <Contact />
+            <Footer />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
